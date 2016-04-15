ae25afd1738f9867ee3e0d1f1ecc56174149ad10
Mohammed Hussein <mhdHussein.dev@gmail.com>
2016-04-15 21:38:58 +0300

    Creating an article using form , validating and showing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,23 @@ class ArticlesController < ApplicationController
   def create
 
     @article = Article.new(article_params)
-    @article.save
 
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "The article is sccessfully created"
+      redirect_to article_path(@article)
+    else
+
+      render 'new'
+    end
+
+
   end
 
+  def show
+
+    @article = Article.find(params[:id])
+
+  end
   private
     def article_params
 
