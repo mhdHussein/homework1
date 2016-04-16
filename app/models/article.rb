7d253903724e43d821eb8597e05d24c3be769aa5
Mohammed Hussein <mhdHussein.dev@gmail.com>
2016-04-16 20:45:54 +0300

    style form partial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 
 class Article < ActiveRecord::Base
 
-  validates :title, presence: true , length: {minimum: 3, maximum: 20}
-  validates :description, presence: true , length: {minimum: 10 , maximum: 300}
+  validates :title, presence: true , length: {minimum: 3 , maximum: 120}
+  validates :description, presence: true , length: {minimum: 10 }
 
 end
