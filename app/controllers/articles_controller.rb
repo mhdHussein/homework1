786071dc05996dc96f8f630e802acd4c2b0ea3d4
Mohammed Hussein <mhdHussein.dev@gmail.com>
2016-04-16 13:12:27 +0300

    removing redundancy from show , edit , update and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 
 class ArticlesController < ApplicationController
 
+  before_action :set_article , only: [:edit,:update,:show,:destroy]
+
+
   def index
 
     @articles = Article.all
@@ -13,7 +16,7 @@ class ArticlesController < ApplicationController
 
   def edit
 
-    @article = Article.find(params[:id])
+
 
   end
 
@@ -34,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def update
 
-    @article = Article.find(params[:id])
+
 
     if @article.update(article_params)
       flash[:notice] = "the article was updated successfuly"
@@ -47,13 +50,13 @@ class ArticlesController < ApplicationController
 
   def show
 
-    @article = Article.find(params[:id])
+
 
   end
 
   def destroy
 
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "the article was deleted successfuly".capitalize!
     redirect_to articles_path
@@ -61,6 +64,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
 
       params.require(:article).permit(:title, :description)
